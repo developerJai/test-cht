f8aaa591bf7c03caadd5a7863a072d36597c9240
Jai <jai@tecorb.co>
2024-08-05 20:30:28 +0530

    Make it better

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :user
+  scope :without_images, -> {
+    where("image is null")
+  }
+
+  scope :clean, -> {
+    without_images.delete_all
+  }
 end
