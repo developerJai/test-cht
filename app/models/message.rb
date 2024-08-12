4abd7ab0f9df4185835a6050fca5ff2f079ff083
Jai <jai@tecorb.co>
2024-08-12 16:36:35 +0530

    Make it better

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,17 @@
 class Message < ApplicationRecord
   belongs_to :user
+  scope :recent_messages, -> {
+    Message.where("created_at>=?", Time.current-12.hour).order(created_at: "DESC")
+  }
+
+  scope :old_messages, -> {
+    Message.where("created_at<?", Time.current-12.hour).order(created_at: "DESC")
+  }
   scope :without_images, -> {
     where("image is null")
   }
 
   scope :clean, -> {
-    without_images.delete_all
+    old_messages.delete_all
   }
 end
